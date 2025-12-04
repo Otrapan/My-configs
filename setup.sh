582496c0e312c48423becc1e2f1273ac7023b37b
Otrapan <markpap978@gmail.com>
2025-12-04 17:32:59 +0200

    setup.sh added

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo pacman -S cmus nvim rofi i3blocks picom nnn kitty fastfetch libmad feh
+sudo pacman -R dmenu
+cd ~/My-configs
+mkdir -p ~/Downloads && sudo cp cof.png ~/Downloads
+sudo cp .config ~/
+echo Succes
