b8ec898c3930b321be35cea65de15bd21da37989
Otrapan <markpap978@gmail.com>
2025-12-05 13:41:38 +0200

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ sudo pacman -S cmus nvim rofi i3blocks picom nnn kitty fastfetch libmad feh
 sudo pacman -R dmenu
 cd ~/My-configs
 mkdir -p ~/Downloads && sudo cp cof.png ~/Downloads
-sudo cp .config ~/
+sudo cp  -r  ~/My-configs/.config ~/
+sudo cp  ~/My-configs/.bashrc ~/
 echo Succes
